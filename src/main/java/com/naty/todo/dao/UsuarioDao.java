2f23da894326c985f072aadcf8401b67ed7d4512
Natalia Medina <natymedina0589@gmail.com>
2022-01-19 15:49:18 -0300

    make create and get usuarios

---
+++ src/main/java/com/naty/todo/dao/UsuarioDao.java
@@ -0,0 +1,13 @@
+package com.naty.todo.dao;
+
+import com.naty.todo.models.Usuario;
+
+import java.util.List;
+
+public interface UsuarioDao {
+
+    List<Usuario> getUsuarios();
+
+    Usuario crearUsuario(Usuario usuario);
+
+}
---
+++ src/main/java/com/naty/todo/dao/UsuarioDaoImp.java
@@ -0,0 +1,31 @@
+package com.naty.todo.dao;
+
+import com.naty.todo.models.Usuario;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.Date;
+import java.util.List;
+
+@Repository
+@Transactional
+public class UsuarioDaoImp implements UsuarioDao {
+
+    @PersistenceContext
+    EntityManager entityManager;
+
+    @Override
+    public List<Usuario> getUsuarios() {
+        String query = "FROM Usuario";
+        return entityManager.createQuery(query).getResultList();
+    }
+
+    @Override
+    public Usuario crearUsuario(Usuario usuario) {
+        entityManager.persist(usuario);
+        return usuario;
+    }
+
+}
