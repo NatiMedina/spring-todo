e3b9409b13f047d2caa79632b95576f2ec90372f
NatiMedina <52016681+NatiMedina@users.noreply.github.com>
2022-01-21 18:45:12 -0300

    Feature/user session (#8)
    * JWT implementation
    
    * create function validarToken for requests
    
    * authorization parameter added to the modicarTodo function

--- src/main/java/com/naty/todo/controllers/TodoController.java
+++ src/main/java/com/naty/todo/controllers/TodoController.java
@@ -2,9 +2,11 @@ package com.naty.todo.controllers;
 
 import com.naty.todo.dao.TodoDao;
 import com.naty.todo.models.Todo;
+import com.naty.todo.utils.JWTUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -13,26 +15,43 @@ public class TodoController {
     @Autowired
     private TodoDao todoDao;
 
+    @Autowired
+    private JWTUtil jwtUtil;
+
     @RequestMapping(value = "api/todo", method = RequestMethod.POST)
-    public Todo createTodo(@RequestBody Todo todo) {
-       return todoDao.crearTodo(todo);
+    public Todo createTodo(@RequestHeader(value = "Authorization") String token, @RequestBody Todo todo) {
+        if (validarToken(token)) {
+            return todoDao.crearTodo(todo);
+        }
+        return null;
     }
 
     @RequestMapping(value = "api/todos", method = RequestMethod.GET)
-    public List<Todo> getTodos() {
+    public List<Todo> getTodos(@RequestHeader(value = "Authorization") String token) {
+        if (!validarToken(token)) {
+            return null;
+        }
         return todoDao.getTodos();
     }
 
+    private boolean validarToken(String token) {
+        String usuarioId = jwtUtil.getKey(token);
+        return usuarioId != null;
+    }
+
     @RequestMapping(value = "api/todo/{id}", method = RequestMethod.DELETE)
-    public void eliminarTodo(@PathVariable Long id) {
-        if (id != null) {
+    public void eliminarTodo(@RequestHeader(value = "Authorization") String token, @PathVariable Long id) {
+        if (validarToken(token) && id != null) {
             todoDao.eliminarTodo(id);
         }
     }
 
     @RequestMapping(value = "api/todo/{id}", method = RequestMethod.PATCH)
-    public Todo modificarTodo(@PathVariable Long id, @RequestBody Todo todo) {
-        return todoDao.modificarTodo(id, todo);
+    public Todo modificarTodo(@RequestHeader(value = "Authorization") String token, @PathVariable Long id, @RequestBody Todo todo) {
+        if (validarToken(token) && id != null) {
+            return todoDao.modificarTodo(id, todo);
+        }
+        return null;
     }
 
 }
